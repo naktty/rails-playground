a76c416051b617177a9f419b434d63558a6d8b7a
nakaty <78027524+nakaty2023@users.noreply.github.com>
2024-11-02 17:07:07 +0900

    railsひながた生成

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ set -e
 # Rails に対応したファイル server.pid が存在しているかもしれないので削除する。
 rm -f /myapp/tmp/pids/server.pid
 
-# コンテナーのプロセスを実行する。（Dockerfile.dev 内の CMD に設定されているもの。）
+# コンテナーのプロセスを実行する。（Dockerfile 内の CMD に設定されているもの。）
 exec "$@"
