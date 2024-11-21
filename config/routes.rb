6ab796ea438f7ff7acf2de83d07b1420d7379bd7
nakaty <78027524+nakaty2023@users.noreply.github.com>
2024-11-21 22:23:13 +0900

    画像表示用のURLをCDN用のURLにする

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,28 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  direct :cdn_image do |model, options|
+    expires_in = options.delete(:expires_in) { ActiveStorage.urls_expire_in }
+
+    if model.respond_to?(:signed_id)
+      route_for(
+        :rails_service_blob_proxy,
+        model.signed_id(expires_in: expires_in),
+        model.filename,
+        options.merge(host: ENV["CDN_HOST"])
+      )
+    else
+      signed_blob_id = model.blob.signed_id(expires_in: expires_in)
+      variation_key  = model.variation.key
+      filename       = model.blob.filename
+
+      route_for(
+        :rails_blob_representation_proxy,
+        signed_blob_id,
+        variation_key,
+        filename,
+        options.merge(host: ENV["CDN_HOST"])
+      )
+    end
+  end
 end
