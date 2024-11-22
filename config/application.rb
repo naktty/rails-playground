75e80157abd465ec9f76c00f98d302a532001b20
nakaty <78027524+nakaty2023@users.noreply.github.com>
2024-11-22 21:00:25 +0900

    Revert "本番環境でactive storageをプロキシモードにする"
    This reverts commit b08ae3684b0de399c4dcfb6e17b586b282c5fca6.

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,6 @@ module Myapp
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
-    config.hosts << ENV["CDN_HOST"]
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
